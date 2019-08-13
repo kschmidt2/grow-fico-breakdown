169f53ca626f9ce9375d07da0ff7feda11dfbb00
Kiersten <kiersten.schmidt@nbcuniversal.com>
2019-08-13 15:46:17 -0400

    adds chart

--- app/js/main.js
+++ app/js/main.js
@@ -23,70 +23,69 @@ Highcharts.setOptions({
 document.addEventListener('DOMContentLoaded', function () {
     const myChart = Highcharts.chart('chart-container', {
         chart: {
-            type: 'bar',
+            type: 'pie',
             styledMode: true,
             spacingBottom: 25,
-            spacingRight: 100
+            spacingLeft: -20,
+            spacingRight: -20
         }, 
+        plotOptions: {
+            pie: {
+                innerSize: '50%'
+            },
+            series: {
+                animation: false,
+                dataLabels: {
+                    crop: false,
+                    distance: '-40%',
+                    useHTML: true,
+                    formatter: function () {
+                        return  '<span class="strong">' + this.point.y + '%</span><br />' + this.point.name;
+                    }
+                }
+            }
+        },  
         title: {
             text: null
-        },
-        data: {
-            googleSpreadsheetKey: '1YOKb5l2VM4aAB2r20N_1aT_1vEajYrP3U-U3A6lZbC0'
-        },
-        // for bar charts only
-        plotOptions: {
-            series: {
-                groupPadding: 0.1
-            } 
-        },
-        legend: {
-            align: 'right',
-            symbolRadius: 0,
-            verticalAlign: 'top',
-            x: 10,
-            itemMarginTop: -10
-        },
-        xAxis: {
-            labels: {
-                style: {
-                    whiteSpace: 'nowrap'
-                }
-            },
-            tickLength: 5
-        },
-        yAxis: {
-            title: false,
-            labels: {
-                useHTML: true,
-                overflow: 'allow'
-            }
         },
         credits: {
             enabled: false
         },
         tooltip: {
-            shadow: false,
-            padding: 10
+            enabled: false
         },
-        responsive: {
-            rules: [{
-              condition: {
-                maxWidth: 500
-              },
-              chartOptions: {
-                chart: {
-                  spacingRight: 10
-                },
-                legend: {
-                    align: 'left',
-                    x: -18
-                },
-                tooltip: {
-                    enabled: false
+        series: [{
+            data: [{
+                name: 'Payment<br>history',
+                y: 35,
+                dataLabels: {
+                    className: 'payhist',
                 }
-              }
-            }]
-        }
+              }, {
+                name: 'Utilization',
+                y: 30,
+                dataLabels: {
+                    className: 'util'
+                }
+              }, {
+                name: 'Length of credit history',
+                y: 15,
+                dataLabels: {
+                    className: 'chist',
+                }
+              }, {
+                name: 'New credit',
+                y: 10,
+                dataLabels: {
+                    className: 'newc',
+                }
+              }, {
+                name: 'Credit mix',
+                y: 10,
+                dataLabels: {
+                    className: 'cmix',
+                }
+              }]
+        }]
     });
 });
